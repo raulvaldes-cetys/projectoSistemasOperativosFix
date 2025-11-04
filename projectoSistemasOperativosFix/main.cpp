26781c54f27e35006e571e0e83e1832001d7693f
raulvaldes-cetys <raul.valdes@cetys.edu.mx>
2025-11-03 20:35:28 -0800

    Initial Commit

---
+++ projectoSistemasOperativosFix/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  projectoSistemasOperativosFix
+//
+//  Created by Raúl Ángel Valdés Corona on 03/11/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
